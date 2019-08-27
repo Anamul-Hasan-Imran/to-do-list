8eeddc8c4b61a9471b5e1fb14e59f6cc680bc1b8
Anamul-Hasan-Imran <imranvoutist@gmail.com>
2019-08-27 17:50:06 +0600

    first commit

---
+++ to-do-list/src/Config/config.js
@@ -0,0 +1,9 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyBcv1p9v51vrfV_GYlM4ip_5hztcmdCSDM",
+    authDomain: "react-project-1bab9.firebaseapp.com",
+    databaseURL: "https://react-project-1bab9.firebaseio.com",
+    projectId: "react-project-1bab9",
+    storageBucket: "react-project-1bab9.appspot.com",
+    messagingSenderId: "469843199670",
+    appId: "1:469843199670:web:bd01812598e9d4d7"
+};
